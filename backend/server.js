ef1a399da1e5780010ca5a4256f6b7408d2978d7
YashChauhan7060 <yc230310@gmail.com>
2026-01-14 17:24:51 +0530

    feat(backend): initialize express server and mongodb connection
    Set up the initial backend infrastructure including:
    - Basic Express app setup with root route.
    - Environment variable configuration via dotenv.
    - Database connection utility using Mongoose.

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import { connectToDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res)=>{
+    res.send("Hello World!");
+});
+
+app.listen(PORT,()=>{
+    connectToDB();
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
