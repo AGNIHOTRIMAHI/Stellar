add7ad0d224b5deb769a362fe59d88acfcad0589
YashChauhan7060 <yc230310@gmail.com>
2026-03-02 17:45:54 +0530

    feat(auth): use environment variable for API_URL

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL + "/api";
 
 export const useAuthStore = create((set) => ({
   user: null,
